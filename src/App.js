98b94f5ae317ea76146dded8f17d58f0b03f1794
Jenny Ewing <jennyewing13@Jennys-MBP.lan>
2020-11-23 21:25:16 -0500

    instructions on login page

--- src/App.js
+++ src/App.js
@@ -53,11 +53,66 @@ class App extends Component {
         </a> 
       </div>
     }
+
+{!this.state.loggedin &&
+      <div>
+         <br></br>
+      </div>
+  }
+
+    {!this.state.loggedin &&
+      <div className="our_mission">
+         <br></br>
+         <p className= "textbox" type = "text" id="mission">We recognize that the connections, resources, and peer-support that many students benefit from are not accessible to everyone.  <br></br> 
+         We celebrate the different circumstances and backgrounds that have led each of you to where you are today: to a community that shares a passion for computer science and cultivates an environment of encouragement and growth. <br></br>
+         Now, the connections and support that you need to succeed are more accessible than ever. </p>
+         <br></br>
+      </div> 
+  }
+  {!this.state.loggedin &&
+      <div>
+         <br></br>
+      </div>
+  }
+  {!this.state.loggedin &&
+
+      <div className="instructions">
+         <br></br>
+         <p className= "textbox" type = "text" id="instruc">Sign into GroupMe to get started. Let's get you connected with your peers!</p>
+         <br></br>
+      </div>
+  }
+
+ 
       
       
     {this.state.loggedin &&
       <div className="App container">
         <SearchPage classes={this.state.classes} tok = {this.state.tok} joinedclasses={this.state.joinedclasses}/>
+      </div>
+    }
+
+    {this.state.loggedin && 
+      <div className="our_mission">
+                    <br></br>
+                    <p className= "textbox" type = "text" id="mission">We recognize that the connections, resources, and peer-support that many students benefit from are not accessible to everyone.  <br></br> 
+                    We celebrate the different circumstances and backgrounds that have led each of you to where you are today: to a community that shares a passion for computer science and cultivates an environment of encouragement and growth. <br></br>
+                    Now, the connections and support that you need to succeed are more accessible than ever. </p>
+                    <br></br>
+      </div> 
+    }
+
+    {this.state.loggedin &&
+      <div>
+                    <br></br>
+      </div>
+    }
+
+    {this.state.loggedin &&
+      <div className="instructions">
+                    <br></br>
+                    <p className= "textbox" type = "text" id="instruc">In the search box above, search for a COMP course (i.e. COMP 110, COMP 401, etc.). <br></br> Click the "click to join" button to be added to a GroupMe for that course with your classmates!</p>
+                    <br></br>
       </div>
     }
     </div>
